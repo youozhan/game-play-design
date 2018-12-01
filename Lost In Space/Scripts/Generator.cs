7a3118ae56ad2404f2452ba254bf8ff1f5acea5b
Youchun <30670835+youozhan@users.noreply.github.com>
2018-12-01 12:29:22 -0500

    New project check-in

---
+++ Lost In Space/Scripts/Generator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Random.Range(-8, 8), 0.5f, Random.Range(-8, 8));
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
